33fbf906f140dddcf437f4b96a718b10b0026ef7
Storm <sotis@ostusa.com>
2020-09-26 14:38:04 -0500

    added playlist add and remove functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -29,8 +29,37 @@ class App extends React.Component {
         album: "Album3",
         id: "3",
       }
-    ]
+    ],
+    playlistName: "",
+    playlistTracks: [
+      {
+        name: "TrackName1",
+        artist: "TrackArtist1",
+        album: "TrackAlbum2",
+        id: "TrackId1",
+      },
+    ],
     }
+
+  this.addTrack = this.addTrack.bind(this);
+  this.removeTrack = this.removeTrack.bind(this);
+  } 
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+        return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -40,8 +69,11 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} 
+              onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
