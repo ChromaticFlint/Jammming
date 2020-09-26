153246f33832bea6da0d34707d8eb6ea97c15eb6
Storm <sotis@ostusa.com>
2020-09-26 12:50:07 -0500

    passed the playlists

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,33 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchResults: [
+      {
+        name: "Name1",
+        artist: "Artist1",
+        album: "Album1",
+        id: "1",
+      },
+      {
+        name: "Name2",
+        artist: "Artist2",
+        album: "Album2",
+        id: "2",
+      },
+      {
+        name: "Name3",
+        artist: "Artist3",
+        album: "Album3",
+        id: "3",
+      }
+    ]
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +40,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             <Playlist />
           </div>
         </div>
